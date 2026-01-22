f4e1eec1ab4716e38b7a289420231507a085fdee
Bruno Marques Caroba <bruno_caroba@hotmail.com>
2026-01-21 21:36:46 -0300

    Add initial implementation for HyCrucible, including bootstrap logic, class transformer management, and Gradle setup.

---
+++ src/main/java/io/github/crucible/hycrucible/bootstrap/annotations/Transformer.java
@@ -0,0 +1,10 @@
+package io.github.crucible.hycrucible.bootstrap.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({java.lang.annotation.ElementType.TYPE})
+public @interface Transformer {
+}
